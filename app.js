fa8a5568da41e5be6ff558d5d44d4f45e8d2d15e
Alan Chou <alanchou19950414@gmail.com>
2022-12-02 11:14:24 +0800

    add gameStatus

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ $(() => {
     let userChooseNumber = new Set();
     let userNumber = [];
     let result = [];
+    let doneStatus = false;
+    let gameStatus = false;
+    if (!gameStatus == false && !doneStatus) {
+
+    }
     const random = () => {
         for (let i = 1; i <= 6; i++) {
-            let x = Math.floor(Math.random() * 49) + 1;
-            if (result.includes(x.toString())) {
+            let x = (Math.floor(Math.random() * 49) + 1).toString();
+            let h = x < 10 ? '0' + x : x;
+            if (result.includes(h)) {
                 i--;
                 continue;
-            } else if (x < 10) {
-                result.push('0' + x)
             } else {
-                result.push(x.toString());
+                result.push(h);
             }
         }
         return result.sort((a, b) => a - b).join(' , ');
@@ -32,46 +36,110 @@ $(() => {
                 background: 'rgb(162, 135, 62)',
             })
         }
-        console.log(userChooseNumber);
-        console.log(result.length)
+
+        //test
+        console.log(userChooseNumber)
     })
 
     $('#done').click(function () {
+        if (doneStatus) {
+            return alert(`請點選重選`);
+        }
         if (userChooseNumber.size < 6) {
             alert(`至少選取六位數字`)
         } else {
             userChooseNumber.forEach((value) => {
                 userNumber.push(value);
             })
-            $('#userChoose').text(`自選號碼：${userNumber.sort((a, b) => a - b).join(' , ')}`)
+            $('#userChoose').text(`自選號碼：
+            ${userNumber.sort((a, b) => a - b).join(' , ')}`)
+            $('td').css({
+                background: '',
+            })
+            doneStatus = true;
+            gameStatus = false;
         }
-        console.log(userNumber)
+
+        //test
+        console.log(`doneStatus = ${doneStatus}`)
+        console.log(`result = ${result}`);
+        console.log(`userNumber = ${userNumber}`)
+        console.log(`userChooseNumber = ${userChooseNumber.size}`)
     })
 
     $('#bet').click(function () {
-        if (userChooseNumber.size < 6) {
-            alert('請先選取號碼下注')
-        } else {
-            $('#betNumber').text(`開獎號碼：${random()}`)
-            console.log(result);
+        if (gameStatus) {
+            return alert(`請重新開始`)
+        }
+        else if (doneStatus === false) {
+            alert(`請先點選下注`)
+        }
+        else {
+            $('#betNumber').text(`開獎號碼：
+            ${random()}`)
+            $('td').css({
+                background: '',
+            })
+            userNumber = [];
+            userChooseNumber = new Set();
             result = [];
-
+            doneStatus = true;
+            gameStatus = true;
         }
 
+        //test
+        console.log(`doneStatus = ${doneStatus}`)
+        console.log(`gameStatus = ${gameStatus}`)
+        console.log(`result = ${result}`);
+        console.log(`userNumber = ${userNumber}`)
+        console.log(`userChooseNumber = ${userChooseNumber.size}`)
     })
 
     $('#reset').click(function () {
-        userChooseNumber;
+        userChooseNumber = new Set();
         userNumber = [];
-        console.log(userChooseNumber);
-        console.log(userNumber)
+        result = [];
+        doneStatus = false;
+        gameStatus = false
+        $('td').css({
+            background: '',
+        })
+        $('#userChoose').text('');
+        $('#betNumber').text('');
+        $('#repeatNumber').text('');
+        $('#autoChoose').text('');
+
+        //test
+        console.log(`doneStatus = ${doneStatus}`)
+        console.log(`result = ${result}`);
+        console.log(`userNumber = ${userNumber}`)
+        console.log(`userChooseNumber = ${userChooseNumber.size}`)
     })
 
+    $('#autoRandom').click(function () {
+        if (doneStatus !== false) {
+            return alert(`請點選重選`)
+        } else {
+            $('#autoChoose').text(`電腦選號：
+            ${random()}`)
+            doneStatus = true;
+            gameStatus = false
+            userNumber = [];
+            result = [];
+        }
+
+        //test
+        console.log(`doneStatus = ${doneStatus}`)
+        console.log(`gameStatus = ${gameStatus}`)
+        console.log(`result = ${result}`);
+        console.log(`userNumber = ${userNumber}`)
+        console.log(`userChooseNumber = ${userChooseNumber.size}`)
+    })
 
     /*
-        1.下注後清空 userNumber && table數字
-        2.隨機選好功能
-        3.重選清空所有功能
+        1.下注後清空 userNumber && table數字 不可繼續堆疊array
+        2.隨機選好功能 {未完成}
+        3.重選清空所有功能 {已完成}
         4.兌獎後清空 userNumber && userChooseNumber && result && table數字 
           並 return 比對後中幾組數字及獲得幾獎 
     */
